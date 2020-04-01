a0ea51adafc741447d8d368884405148a85af665
Ngoc Minh Tran <M.N.Tran@ibm.com>
2020-04-01 14:00:50 +0100

    imp main pgd
    Signed-off-by: Ngoc Minh Tran <M.N.Tran@ibm.com>

--- art/attacks/evasion/projected_gradient_descent/projected_gradient_descent.py
+++ art/attacks/evasion/projected_gradient_descent/projected_gradient_descent.py
@@ -29,9 +29,7 @@ import logging
 
 import numpy as np
 
-from art.attacks.evasion.adversarial_patch.adversarial_patch_numpy import AdversarialPatchNumpy
-from art.attacks.evasion.adversarial_patch.adversarial_patch_tensorflow import AdversarialPatchTensorFlowV2
-from art.classifiers import TensorFlowV2Classifier
+from art.classifiers import TensorFlowClassifier
 from art.classifiers.classifier import ClassifierNeuralNetwork, ClassifierGradients
 from art.attacks.attack import EvasionAttack
 from art.exceptions import ClassifierError
@@ -50,168 +48,99 @@ class ProjectedGradientDescent(EvasionAttack):
     """
 
     attack_params = EvasionAttack.attack_params + [
-        "rotation_max",
-        "scale_min",
-        "scale_max",
-        "learning_rate",
-        "max_iter",
+        "norm",
+        "eps",
+        "eps_step",
+        "targeted",
+        "num_random_init",
         "batch_size",
+        "minimal",
+        "max_iter",
+        "random_eps"
     ]
 
     def __init__(
         self,
         classifier,
-        rotation_max=22.5,
-        scale_min=0.1,
-        scale_max=1.0,
-        learning_rate=5.0,
-        max_iter=500,
-        batch_size=16,
-        patch_shape=None,
+        norm=np.inf,
+        eps=0.3,
+        eps_step=0.1,
+        max_iter=100,
+        targeted=False,
+        num_random_init=0,
+        batch_size=1,
+        random_eps=False
     ):
         """
-        Create an instance of the :class:`.AdversarialPatch`.
+        Create a :class:`.ProjectedGradientDescent` instance.
 
         :param classifier: A trained classifier.
         :type classifier: :class:`.Classifier`
-        :param rotation_max: The maximum rotation applied to random patches. The value is expected to be in the
-               range `[0, 180]`.
-        :type rotation_max: `float`
-        :param scale_min: The minimum scaling applied to random patches. The value should be in the range `[0, 1]`,
-               but less than `scale_max`.
-        :type scale_min: `float`
-        :param scale_max: The maximum scaling applied to random patches. The value should be in the range `[0, 1]`, but
-               larger than `scale_min.`
-        :type scale_max: `float`
-        :param learning_rate: The learning rate of the optimization.
-        :type learning_rate: `float`
-        :param max_iter: The number of optimization steps.
+        :param norm: The norm of the adversarial perturbation. Possible values: np.inf, 1 or 2.
+        :type norm: `int`
+        :param eps: Maximum perturbation that the attacker can introduce.
+        :type eps: `float`
+        :param eps_step: Attack step size (input variation) at each iteration.
+        :type eps_step: `float`
+        :param random_eps: When True, epsilon is drawn randomly from truncated normal distribution. The literature
+                           suggests this for FGSM based training to generalize across different epsilons. eps_step
+                           is modified to preserve the ratio of eps / eps_step. The effectiveness of this
+                           method with PGD is untested (https://arxiv.org/pdf/1611.01236.pdf).
+        :type random_eps: `bool`
+        :param max_iter: The maximum number of iterations.
         :type max_iter: `int`
-        :param batch_size: The size of the training batch.
+        :param targeted: Indicates whether the attack is targeted (True) or untargeted (False)
+        :type targeted: `bool`
+        :param num_random_init: Number of random initialisations within the epsilon ball. For num_random_init=0
+            starting at the original input.
+        :type num_random_init: `int`
+        :param batch_size: Size of the batch on which adversarial samples are generated.
         :type batch_size: `int`
-        :param patch_shape: The shape of the adversarial path as a tuple of shape (width, height, nb_channels).
-                            Currently only supported for `TensorFlowV2Classifier`. For classifiers of other frameworks
-                            the patch_shape is set to the shape of the image samples.
-        :type patch_shape: (`int`, `int`, `int`)
         """
-        super(AdversarialPatch, self).__init__(classifier=classifier)
+        super(ProjectedGradientDescent, self).__init__(classifier=classifier)
         if not isinstance(classifier, ClassifierNeuralNetwork) or not isinstance(classifier, ClassifierGradients):
             raise ClassifierError(self.__class__, [ClassifierNeuralNetwork, ClassifierGradients], classifier)
 
-        assert (
-            self.classifier.clip_values is not None
-        ), "Adversarial Patch attack requires a classifier with clip_values."
-
-        if isinstance(self.classifier, TensorFlowV2Classifier):
-            self._attack = AdversarialPatchTensorFlowV2(
+        if isinstance(self.classifier, TensorFlowClassifier):
+            self._attack = ProjectedGradientDescentTensorFlow(
                 classifier=classifier,
-                rotation_max=rotation_max,
-                scale_min=scale_min,
-                scale_max=scale_max,
-                learning_rate=learning_rate,
+                norm=norm,
+                eps=eps,
+                eps_step=eps_step,
                 max_iter=max_iter,
+                targeted=targeted,
+                num_random_init=num_random_init,
                 batch_size=batch_size,
-                patch_shape=patch_shape,
+                random_eps=random_eps
             )
         else:
-            self._attack = AdversarialPatchNumpy(
+            self._attack = ProjectedGradientDescentNumpy(
                 classifier=classifier,
-                rotation_max=rotation_max,
-                scale_min=scale_min,
-                scale_max=scale_max,
-                learning_rate=learning_rate,
+                norm=norm,
+                eps=eps,
+                eps_step=eps_step,
                 max_iter=max_iter,
+                targeted=targeted,
+                num_random_init=num_random_init,
                 batch_size=batch_size,
+                random_eps=random_eps
             )
 
     def generate(self, x, y=None, **kwargs):
         """
         Generate adversarial samples and return them in an array.
 
-        :param x: An array with the original inputs. `x` is expected to have spatial dimensions.
+        :param x: An array with the original inputs.
         :type x: `np.ndarray`
-        :param y: An array with the original labels to be predicted.
+        :param y: Target values (class labels) one-hot-encoded of shape `(nb_samples, nb_classes)` or indices of shape
+                  (nb_samples,). Only provide this parameter if you'd like to use true labels when crafting adversarial
+                  samples. Otherwise, model predictions are used as labels to avoid the "label leaking" effect
+                  (explained in this paper: https://arxiv.org/abs/1611.01236). Default is `None`.
         :type y: `np.ndarray`
-        :return: An array holding the adversarial patch.
+        :return: An array holding the adversarial examples.
         :rtype: `np.ndarray`
         """
-        logger.info("Creating adversarial patch.")
-
-        assert y is not None, "Adversarial Patch attack requires target values `y`."
-
-        if len(x.shape) == 2:
-            raise ValueError(
-                "Feature vectors detected. The adversarial patch can only be applied to data with spatial "
-                "dimensions."
-            )
+        logger.info("Creating adversarial samples.")
 
         return self._attack.generate(x=x, y=y, **kwargs)
 
-    def apply_patch(self, x, scale):
-        """
-        A function to apply the learned adversarial patch to images.
-
-        :param x: Instances to apply randomly transformed patch.
-        :type x: `np.ndarray`
-        :param scale: Scale of the applied patch in relation to the classifier input shape.
-        :type scale: `float`
-        :return: The patched instances.
-        :rtype: `np.ndarray`
-        """
-        return self._attack.apply_patch(x, scale)
-
-    def set_params(self, **kwargs):
-        """
-        Take in a dictionary of parameters and applies attack-specific checks before saving them as attributes.
-
-        :param rotation_max: The maximum rotation applied to random patches. The value is expected to be in the
-               range `[0, 180]`.
-        :type rotation_max: `float`
-        :param scale_min: The minimum scaling applied to random patches. The value should be in the range `[0, 1]`,
-               but less than `scale_max`.
-        :type scale_min: `float`
-        :param scale_max: The maximum scaling applied to random patches. The value should be in the range `[0, 1]`,
-               but greater than `scale_min`.
-        :type scale_max: `float`
-        :param learning_rate: The learning rate of the optimization.
-        :type learning_rate: `float`
-        :param max_iter: The number of optimization steps.
-        :type max_iter: `int`
-        :param batch_size: The size of the training batch.
-        :type batch_size: `int`
-        """
-        super(AdversarialPatch, self).set_params(**kwargs)
-
-        if not isinstance(self._attack.rotation_max, (float, int)):
-            raise ValueError("The maximum rotation of the random patches must be of type float.")
-        if self._attack.rotation_max < 0 or self._attack.rotation_max > 180.0:
-            raise ValueError("The maximum rotation of the random patches must be between 0 and 180 degrees.")
-
-        if not isinstance(self._attack.scale_min, float):
-            raise ValueError("The minimum scale of the random patched must be of type float.")
-        if self._attack.scale_min < 0 or self._attack.scale_min >= self._attack.scale_max:
-            raise ValueError(
-                "The minimum scale of the random patched must be greater than 0 and less than the maximum scaling."
-            )
-
-        if not isinstance(self._attack.scale_max, float):
-            raise ValueError("The maximum scale of the random patched must be of type float.")
-        if self._attack.scale_max > 1:
-            raise ValueError("The maximum scale of the random patched must not be greater than 1.")
-
-        if not isinstance(self._attack.learning_rate, float):
-            raise ValueError("The learning rate must be of type float.")
-        if not self._attack.learning_rate > 0.0:
-            raise ValueError("The learning rate must be greater than 0.0.")
-
-        if not isinstance(self._attack.max_iter, int):
-            raise ValueError("The number of optimization steps must be of type int.")
-        if not self._attack.max_iter > 0:
-            raise ValueError("The number of optimization steps must be greater than 0.")
-
-        if not isinstance(self._attack.batch_size, int):
-            raise ValueError("The batch size must be of type int.")
-        if not self._attack.batch_size > 0:
-            raise ValueError("The batch size must be greater than 0.")
-
-        return True
